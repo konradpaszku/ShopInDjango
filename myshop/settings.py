bdece79f9e7d95bd8e5df88ecee5d858afea89d3
Konrad <konrad.paszek@icloud.com>
2021-01-25 17:38:05 +0100

    Create 'cart' for shopping with functions add/remove, add templates for cart.

--- myshop/settings.py
+++ myshop/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     #myapp
     'shop',
+    'cart',
 ]
 
 MIDDLEWARE = [
@@ -125,3 +126,6 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+SESSION_EXPIRE_AT_BROWSER_CLOSE = True
+CART_SESSION_ID = 'cart'
